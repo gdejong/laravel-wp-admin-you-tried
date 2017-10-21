2c8acc67516203af74f0a17936fbba7d3ee43c75
gdejong <gijs_d.j@hotmail.com>
2017-10-21 20:11:33 +0200

    Make sure the foreach receives an array

--- src/routes.php
+++ src/routes.php
@@ -7,14 +7,16 @@
 |
 */
 
-foreach (config('wp-admin-you-tried.login-endpoints') as $endpoint) {
-    Route::get($endpoint, function () {
-        return view('gdejong::index');
-    });
-
-    if (config('wp-admin-you-tried.action') === 'redirect') {
-        Route::post($endpoint, function () {
-            return redirect()->to(config('wp-admin-you-tried.redirect_url'));
+if(is_array(config('wp-admin-you-tried.login-endpoints'))) {
+     foreach (config('wp-admin-you-tried.login-endpoints') as $endpoint) {
+        Route::get($endpoint, function () {
+            return view('gdejong::index');
         });
-    }
+
+        if (config('wp-admin-you-tried.action') === 'redirect') {
+            Route::post($endpoint, function () {
+                return redirect()->to(config('wp-admin-you-tried.redirect_url'));
+            });
+        }
+    }   
 }
